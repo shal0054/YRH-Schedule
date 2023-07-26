1d3fc7c18f29abcbdd45b5df14cd7cb7ed5353b9
Karim Shaloh <karimshaloh@Karims-MacBook-Pro.local>
2023-07-26 01:28:07 -0400

    Add 'Date' to row headings

--- src/components/ScheduleData.jsx
+++ src/components/ScheduleData.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Admin } from '../pages/Admin';
 
 function ScheduleData() {
 	const [scheduleData, setScheduleData] = useState(null);
+
+	useEffect(() => {
+		if (scheduleData)
+			setScheduleData(prevData => {
+				let newData = [...prevData, (prevData[0][0] = 'Date')];
+				return newData;
+			});
+	}, []);
+
 	return (
 		<>
 			<Admin setScheduleData={setScheduleData} />
-			{scheduleData && <div>{scheduleData}</div>}
+			{scheduleData && <div>{scheduleData[0][0]}</div>}
 		</>
 	);
 }
